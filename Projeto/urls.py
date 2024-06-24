08c13503ff55a72698af379fb34a91961efe99e7
Vitória Raupp <vitoriaraupp0@gmail.com>
2024-06-24 15:19:32 -0300

    Update urls.py

--- Projeto/urls.py
+++ Projeto/urls.py
@@ -8,26 +8,16 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index', views.index, name='index'),
-    # path('', include('core.urls')),
+    #path('', include('core.urls')),
     path('', views.index),
     path('brasil/', views.pagina_brasil),
+    path('compare/', views.pagina_compare),
     path('historico/', views.pagina_historico),
-    path('login/', views.pagina_login, name='login.html'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/register/', views.register, name='register'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('perfil/', views.pagina_perfil),
     path('teste/', views.teste),
-    path('termos/', views.termos, name='termos.html'),
-    path('charts/', views.pagina_charts, name='charts.html'),
-    path('contato', views.contato, name='contato.html'),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('Projeto.urls')),
-
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('Projeto.urls')),
-
 ]
 
 urlpatterns += staticfiles_urlpatterns()
